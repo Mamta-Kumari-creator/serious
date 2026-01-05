107c76f438294a2fb47ff0915d914fa0d2448260
Mamta-Kumari-creator <23f2001943@ds.study.iitm.ac.in>
2026-01-05 14:57:24 +0530

    customer and manager role

--- main.py
+++ main.py
@@ -10,11 +10,34 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-    admin_role=Role.query.get.filter_by(name='Admin').first()
+    admin_role=Role.query.filter_by(name='Admin').first()
     if not admin_role:
         admin_role = Role(name='Admin')
         db.session.add(admin_role)
-        db.session.commit()
+
+    cust_role=Role.query.filter_by(name='Customer').first()
+    if not cust_role:
+        cust_role = Role(name='Customer')
+        db.session.add(cust_role)
+
+    manager_role=Role.query.filter_by(name='Manager').first()
+    if not manager_role:
+        manager_role = Role(name='Manager')
+        db.session.add(manager_role)
+
+    admin_user=User.query.filter_by(email='admin@gmail.com').first()
+    if not admin_user:
+        admin_user = User(
+            name='Admin User',
+            email='admin@gmail.com',
+            password='admin123',
+            role=admin_role
+        )
+        db.session.add(admin_user)
+
+    db.session.commit()
+    
+
 
 @app.route('/')
 def hello_world():
