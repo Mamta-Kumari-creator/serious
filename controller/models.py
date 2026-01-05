325804d52828913295db3049bad81edf19ae2d8d
Mamta-Kumari-creator <23f2001943@ds.study.iitm.ac.in>
2026-01-05 14:48:05 +0530

    admin role added

---
+++ controller/models.py
@@ -0,0 +1,33 @@
+from controller.database import db
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    customers= db.relationship('Customer', backref='user', lazy=True,uselist=False)
+    store_manager_details= db.relationship('StoreManager', backref='user', lazy=True,uselist=False)
+
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+class UserRole(db.Model):
+    #table name will be "user_role"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)   
+
+class StoreManager(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    
+    qualification = db.Column(db.String(120), nullable=True)
+    
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    address = db.Column(db.String(200), nullable=True)
+    preffered_mode_of_payment = db.Column(db.String(50), nullable=True)
+    phone_number = db.Column(db.String(20), nullable=True)
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from controller.database import db
 from controller.config import config
+from controller.models import *
+
 app = Flask(__name__, template_folder='templates',static_folder='static')
 app.config.from_object(config)
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
-    
+
+    admin_role=Role.query.get.filter_by(name='Admin').first()
+    if not admin_role:
+        admin_role = Role(name='Admin')
+        db.session.add(admin_role)
+        db.session.commit()
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
